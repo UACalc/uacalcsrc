95c683f51c67d1ecb12508a40e0faeee411d9fe1
Ralph Freese <ralph@math.hawaii.edu>
2007-01-08 08:47:06 +0000

    Task Manager.

---
+++ org/uacalc/ui/tm/Task.java
@@ -0,0 +1,7 @@
+package org.uacalc.ui.tm;
+
+public interface Task<T> {
+  
+  public T doIt();
+
+}
---
+++ org/uacalc/ui/tm/TaskRunner.java
@@ -0,0 +1,113 @@
+package org.uacalc.ui.tm;
+
+import javax.swing.SwingWorker;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import javax.swing.BorderFactory;
+import java.awt.event.*;
+import org.uacalc.alg.*;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class TaskRunner<T, V> extends SwingWorker<T, V> {
+  
+  Task<T> task;
+  
+  public TaskRunner(Task<T> task) {
+    this.task = task;
+  }
+  
+  public T doInBackground() {
+    return task.doIt();
+  }
+  
+  public void done() {
+    Toolkit.getDefaultToolkit().beep();
+    //startButton.setEnabled(true);
+    //setCursor(null); //turn off the wait cursor
+    //output.append("Done!\n");
+  }
+
+  
+  public static void main(String[] args) {
+    final JTextArea output = new JTextArea(10,50);
+    output.setMargin(new Insets(5,5,5,5));
+    output.setEditable(false);
+    List<Operation> ops = new ArrayList<Operation>();
+    Operation meet = new AbstractOperation("meet", 2, 2) {
+
+      public Object valueAt(List args) {
+        throw new UnsupportedOperationException();
+      }
+
+      // ordinary meet
+      public int intValueAt(int[] args) {
+        return Math.min(args[0], args[1]);
+      }
+    };
+    ops.add(meet);
+    final SmallAlgebra semilat = new BasicAlgebra("semilat", 2, ops);
+    final Task<Integer> task = new Task<Integer>() {
+      public Integer doIt() {
+        FreeAlgebra freeSemilattice = new FreeAlgebra(semilat, 4);
+        return freeSemilattice.con().cardinality();
+      }
+    };
+    final TaskRunner<Integer, String> runner = 
+            new TaskRunner<Integer, String>(task);
+    final Integer ans;
+    JPanel panel = new JPanel(new BorderLayout());
+    JButton startButton = new JButton("Start");
+    startButton.setActionCommand("start");
+    startButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          runner.execute();
+          output.append("Done!\n");
+          output.append("Answer\n");
+          try {
+            output.append(runner.get().toString());
+            output.append("\n\n\n");
+          }
+          catch (Exception ex) {ex.printStackTrace(); }
+        }
+      });  
+    
+    JButton cancelButton  = new JButton("Cancel");
+    cancelButton.setActionCommand("cancel");
+    cancelButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          if (task != null) {
+            runner.cancel(true);
+          }
+        }
+      });
+
+    JPanel buttonPanel = new JPanel();
+    buttonPanel.add(startButton);
+    buttonPanel.add(cancelButton);
+    //buttonPanel.add(progressBar);
+
+    panel.add(buttonPanel, BorderLayout.PAGE_START);
+    panel.add(new JScrollPane(output), BorderLayout.CENTER);
+    panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+    
+    JFrame frame = new JFrame("Task Runner Demo");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    frame.setContentPane(panel);
+
+    //Display the window.
+    frame.pack();
+    frame.setVisible(true);
+
+
+  }
+  
+}
