4803e638e7a39c1f6da450bf1cbcb7ef193d4dd2
Ralph Freese <ralph@math.hawaii.edu>
2011-05-16 18:36:00 +0000

    algebras with a specified minimal set.

--- org/uacalc/alg/conlat/BasicPartition.java
+++ org/uacalc/alg/conlat/BasicPartition.java
@@ -1313,12 +1313,12 @@ public class BasicPartition extends IntArray implements Partition, Comparable {
 
     // [|0|1|2|3|4|5|6,7,8|, |0,3,7|1,5,8|2,4,6|, |0,5,6|1,4,7|2,3,8|, |0,3|1,5|2,4|6|7|8|]
     // con size = 16
-    /*
+    
     BasicPartition alpha = new BasicPartition(new int[] {-1, -1, -1, -1, -1, -1, -3, 6, 6});
     BasicPartition beta  = new BasicPartition(new int[] {-3, -3, -3, 0, 2, 1, 2, 0, 1});
     BasicPartition gamma = new BasicPartition(new int[] {-3, -3, -3, 2, 1, 0, 0, 1, 2});
     BasicPartition delta = new BasicPartition(new int[] {-2,  -2, -2, 0, 2, 1, -1, -1, -1});
-    */
+    
     
     // Double Wing Pentagon:
     // |0|1,2|3,4|5,6|, |0,2,4|1,6|3|5|", |0,1,3,5|2|4|6|, |0,1,3,5|2|4,6|
@@ -1353,11 +1353,11 @@ public class BasicPartition extends IntArray implements Partition, Comparable {
     BasicPartition delta = new BasicPartition("|0 5 10|1 4 14|2 3|6 8|7 9|11 13|12 15|");
     */
     
-    BasicPartition alpha = new BasicPartition("| 0 1 2| 3 4 5 |6 7 8|");
-    BasicPartition beta  = new BasicPartition("|0 3|1|2|4 6|5|7|8|");
-    BasicPartition gamma = new BasicPartition("|0|1 4|2|3|5 8|6|7|");
-    //BasicPartition delta = new BasicPartition("|0 5 8|1 4 7|2|3|6|");
-    BasicPartition delta = new BasicPartition("[[0 5 8],[1 4 7],[2],[3],[6]]");
+    //BasicPartition alpha = new BasicPartition("| 0 1 2| 3 4 5 |6 7 8|");
+    //BasicPartition beta  = new BasicPartition("|0 3|1|2|4 6|5|7|8|");
+    //BasicPartition gamma = new BasicPartition("|0|1 4|2|3|5 8|6|7|");
+    ////BasicPartition delta = new BasicPartition("|0 5 8|1 4 7|2|3|6|");
+    //BasicPartition delta = new BasicPartition("[[0 5 8],[1 4 7],[2],[3],[6]]");
     
     ////////////////////////////////////////////////////////////////
     //       A Triple Wing Pentagon
