26ac048f07453127044a26e1f417e184bcaac39f
Ralph Freese <ralph@math.hawaii.edu>
2014-08-29 08:25:31 +0000

    work on type 1.

---
+++ org/uacalc/example/Type1.java
@@ -0,0 +1,56 @@
+package org.uacalc.example;
+
+import org.uacalc.alg.*;
+import org.uacalc.alg.op.*;
+import org.uacalc.util.*;
+import org.uacalc.terms.*;
+import java.util.*;
+
+
+public class Type1 {
+  
+  public static SmallAlgebra reductAlg (SmallAlgebra alg) {
+    SmallAlgebra matpow = Algebras.matrixPower(alg, 3);
+    BigProductAlgebra bigProd = new BigProductAlgebra(matpow, 4);
+    List<IntArray> gens = new ArrayList<>();
+    IntArray g0 = new IntArray(new int[] {0,0,1,1});
+    IntArray g1 = new IntArray(new int[] {0,1,0,1});
+    final Map<IntArray, Term> termMap = new HashMap<IntArray, Term>();
+    List<Variable> varList = new ArrayList<>();   
+    termMap.put(g0, Variable.x);
+    termMap.put(g1, Variable.y);
+    varList.add(Variable.x);
+    varList.add(Variable.y);
+    gens.add(g0);
+    gens.add(g1);    
+    for (int i = 0; i < matpow.cardinality(); i++) {
+      IntArray c = new IntArray(new int[] {i,i,i,i});
+      gens.add(c);
+      Variable var = new VariableImp("c" + i);
+      termMap.put(c, var);
+      varList.add(var);
+    }
+    Closer closer = new Closer(bigProd, gens, termMap);
+    List<IntArray> lst = closer.close();
+    IntArray ia0 = new IntArray(new int[]{0,1,2,3});
+    IntArray ia1 = new IntArray(new int[]{0,4,3,7});
+    Term t0 = termMap.get(ia0);
+    Term t1 = termMap.get(ia1);
+    Operation h = t0.interpretation(matpow, varList, false);
+    Operation k = t1.interpretation(matpow, varList, false);
+    List<Operation> ops = new ArrayList<>(2);
+    ops.add(h);
+    ops.add(k);
+    SmallAlgebra ans = new BasicAlgebra("reduct", matpow.cardinality(), ops);
+    return ans;
+  }
+  
+  
+
+  public static void main(String[] args) {
+    SmallAlgebra two = new BasicAlgebra("two", 2, new ArrayList<Operation>());
+    SmallAlgebra ans = reductAlg(two);
+
+  }
+
+}
