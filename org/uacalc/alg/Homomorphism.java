d317f0915f694e29ff61bcc5ff8104e9ed645dd6
ralphfreese <ralph@math.hawaii.edu>
2014-12-01 22:15:22 -1000

    A class to represent homomorphisms.

---
+++ org/uacalc/alg/Homomorphism.java
@@ -0,0 +1,68 @@
+package org.uacalc.alg;
+
+import java.util.*;
+
+import org.uacalc.alg.conlat.*;
+
+/**
+ * A homomorphism from the domain algebra into
+ * the range algebra.
+ * 
+ * @author ralph
+ *
+ * @param <K>  the type of element of the domain
+ * @param <V>  the type of element of the range
+ */
+public class Homomorphism<K,V> {
+  
+  private SmallAlgebra domain;
+  private SmallAlgebra range;
+  private Map<K,V> map;
+  
+  public Homomorphism(SmallAlgebra domain, SmallAlgebra range, Map<K,V> map) {
+    this.setDomain(domain);
+    this.setRange(range);
+    this.setMap(map);
+  }
+  
+  public Partition kernel() {
+    final int size = domain.cardinality();
+    Partition par = BasicPartition.zero(size);
+    for (int i = 0; i < size; i++) {
+      for (int j = i+1; j < size; j++) {
+        if (map.get(domain.getElement(i)) == map.get(domain.getElement(j))) {
+          // here
+        }
+      }
+    }
+    
+    // TODO: complete this
+    return par;
+  }
+
+  public Algebra getDomain() {
+    return domain;
+  }
+
+  public void setDomain(SmallAlgebra domain) {
+    this.domain = domain;
+  }
+
+  public SmallAlgebra getRange() {
+    return range;
+  }
+
+  public void setRange(SmallAlgebra range) {
+    this.range = range;
+  }
+
+  public Map<K,V> getMap() {
+    return map;
+  }
+
+  public void setMap(Map<K,V> map) {
+    this.map = map;
+  }
+  
+
+}
