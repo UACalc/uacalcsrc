7b705c192a3e5df96ef4dafffaca9fee090b69e7
williamdemeo <williamdemeo@gmail.com>
2015-01-10 03:23:07 -0600

    Cleaned up Java 8 example and added algebra files used, so the example can be run by others.

--- org/uacalc/example/FilteringAlgebras.java
+++ org/uacalc/example/FilteringAlgebras.java
@@ -6,32 +6,30 @@ import java.util.*;
 import org.uacalc.alg.*;
 import org.uacalc.io.AlgebraIO;
 
+
+// This class demonstrates filtering a list of algebras using some predicate.
+// In the example below we use "non-congruence-SD-meet" as the predicate. 
+// That is, we read in some SmallAlgebras, storing them all in a List<Algebra> object,
+// then apply a filter that returns a list containing only those that are NOT SD-meet.
 public class FilteringAlgebras {
 
 	public static void main(String ... args) throws Exception {
 
-		// List<String> filenames = Arrays.asList("alg1", "alg2");
-		// SmallAlgebra alg = org.uacalc.io.AlgebraIO.readAlgebraFile("/Users/ralph/Java/Algebra/algebras/emil2y.ua");
+		String algebras_dir = "resources/algebras/bergman/";
 
-		String algebra_dir = "../UACalc-Team/AlgebraFiles/Bergman/CIB4SL/";
+		// A list of indices of some algebras we'll read in:
+		List<Integer> algebra_indices = Arrays.asList(1, 100, 200, 201, 205, 217, 233, 457, 500, 1017);
 
-		
-		List<Algebra> algebras = Arrays.asList(
-				AlgebraIO.readAlgebraFile(algebra_dir + "CIB4-1.ua"),
-				AlgebraIO.readAlgebraFile(algebra_dir + "CIB4-100.ua"),
-				AlgebraIO.readAlgebraFile(algebra_dir + "CIB4-200.ua"),
-				AlgebraIO.readAlgebraFile(algebra_dir + "CIB4-201.ua"),
-				AlgebraIO.readAlgebraFile(algebra_dir + "CIB4-205.ua"),
-				AlgebraIO.readAlgebraFile(algebra_dir + "CIB4-217.ua"),
-				AlgebraIO.readAlgebraFile(algebra_dir + "CIB4-233.ua"),
-				AlgebraIO.readAlgebraFile(algebra_dir + "CIB4-457.ua"),
-				AlgebraIO.readAlgebraFile(algebra_dir + "CIB4-500.ua"),
-				AlgebraIO.readAlgebraFile(algebra_dir + "CIB4-1017.ua")
-				);
-		// Of those listed above, 201, 217, 233, 457, 1017 are non-SD-meet algebras.
-		// Ralph's method,
-		//    Malcev.sdMeetIdempotent((SmallAlgebra) a, null) 
-		// returns an IntArray witnessing the failure of sdMeet, and null if no failure.
+		// A list of algebras specified in files CIB4-1.ua, CIB4-100.ua, CIB4-200.ua, etc.
+		List<Algebra> algebras = new ArrayList<>();
+		for (Integer i : algebra_indices) {
+				algebras.add(AlgebraIO.readAlgebraFile(algebras_dir + "CIB4-" + i + ".ua"));
+		}
+
+		// Of those algebras in the list above, we know from earlier experiments that 
+		// those with indices 201, 217, 233, 457, 1017 are non-SD-meet algebras.
+		// The method Malcev.sdMeetIdempotent returns an IntArray witnessing the 
+		// failure of sdMeet, and null if there is no failure.
 		// That is, null is returned when the algebra is congruence SD-meet.
 		// Therefore, to filter for non-SD-meet algebras, check when sdMeetIdempotent is non-null.
 		List<Algebra> nonSDmeet = filter(algebras, (Algebra a) -> (Malcev.sdMeetIdempotent((SmallAlgebra) a, null)!=null) );
@@ -50,4 +48,8 @@ public class FilteringAlgebras {
 		}
 		return result;
 	}
+	
+	// Reference: "Java 8 in Action" in particular, see the 
+	//            lambdasinaction.chap3 package of the repository at 
+	//            https://github.com/java8/Java8InAction 
 }
